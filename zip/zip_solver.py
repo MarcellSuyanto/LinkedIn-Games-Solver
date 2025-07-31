b0d3556398b142c8123fe07025550d6758cd042f
Elysium-6 <cursed.diamondking@gmail.com>
2025-07-31 21:22:45 +0800

    v1 prototype

--- zip/zip_solver.py
+++ zip/zip_solver.py
@@ -1,3 +1,4 @@
+import time
 
 class Cell:
 
@@ -32,23 +33,47 @@ class Zip:
         print("Solving LinkedIn ZIP minigame")
         self.map = Map()
         self.map.show_map()
-        self.paths = [[] for i in range(self.map.size)]
+        self.paths = [[] for i in range(len(self.map.checkpoints) + 1)]
         self.directions = [(-1,0), (1,0), (0,-1), (0,1)]
+        self.route = []
 
     def find_paths(self):
         for i in range(1, len(self.map.checkpoints)):
-            self.dfs(self.map.checkpoints[i-1][0], self.map.checkpoints[i-1][1], self.map.checkpoints[i][0], self.map.checkpoints[i][1], [], i)
+            self.path_dfs(self.map.checkpoints[i-1][0], self.map.checkpoints[i-1][1], self.map.checkpoints[i][0], self.map.checkpoints[i][1], [], i)
+            print(f'Section {i} done')
         # print(self.paths)
-        print("Number of paths: {}".format(len(self.paths[1])))
+        print("Number of paths: {}".format(sum(len(x) for x in self.paths)))
 
-    def dfs(self, cx, cy, ex, ey, path, index):
+    def evaluate(self):
+        route = []
+        print(self.eval_dfs(1, route))
+
+    def eval_dfs(self, section, route):
+        if section == len(self.map.checkpoints):
+            self.route = list(route)
+        else:
+            for path in self.paths[section]:
+                overlap = False
+                for cell in path:
+                    if cell in route or cell in self.map.checkpoints[0:section] + self.map.checkpoints[section+1:]:
+                        overlap = True
+                        break
+                if not overlap:
+                    for cell in path:
+                        route.append(cell)
+                    self.eval_dfs(section + 1, route)
+                    for cell in path:
+                        route.pop(-1)
+
+
+    def path_dfs(self, cx, cy, ex, ey, path, index):
         if (cx == ex) and (cy == ey):
             self.paths[index].append(list(path))
         else:
             for dx, dy in self.directions:
                 if (0 <= cx+dx) and (cx+dx < self.map.size) and (0 <= cy+dy) and (cy+dy < self.map.size) and ((cx+dx,cy+dy) not in path) and len(path) < self.map.size**2 * 0.8:
                     path.append((cx+dx, cy+dy))
-                    self.dfs(cx+dx, cy+dy, ex, ey, path, index)
+                    self.path_dfs(cx+dx, cy+dy, ex, ey, path, index)
                     path.pop(-1)
 
     def show_path(self, path):
@@ -61,11 +86,14 @@ class Zip:
 
 
 test = Zip()
+print("Finding Paths...")
 test.find_paths()
-for path in test.paths[2]:
-    print(path)
-    test.show_path(path)
-
-print(f'Section 1: {len(test.paths[1])} paths')
-print(f'Section 2: {len(test.paths[2])} paths')
+for i in range(1, len(test.paths) - 1):
+    print(f'Section {i}: {len(test.paths[i])} paths')
+print("Evaluating...")
+start = time.time()
+test.evaluate()
+elap = time.time() - start
+print(test.route)
+print(f'Evaluation time: {elap}')
 
